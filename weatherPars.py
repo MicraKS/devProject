011950b1696c90f4438990c5c1fb2c8e23d3a11e
MicraKS <5328207@gmail.com>
2023-06-09 15:40:45 +0300

    Ready

--- weatherPars.py
+++ weatherPars.py
@@ -1,20 +1,22 @@
 import requests
 
 
-def weather_forecast(city):
+def give_weather(city):
     params = {"lang": "ru",
               "M": "",
               "n": "",
               "q": "",
               "T": "",
               }
+
     url = f'https://wttr.in/{city}'
 
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.text
 
+
 if __name__ == '__main__':
-    print(weather_forecast("Лондон"))
-    print(weather_forecast("Аэропорт Шереметьево"))
-    print(weather_forecast("Череповец"))
+    places = ["Лондон", "SVO", "Череповец"]
+    for place in places:
+        print(give_weather(place))
